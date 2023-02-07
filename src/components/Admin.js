fb0e5548fb003fe0bd783859f48041e3abaa1d4e
Mohammad Mir <mirabdollahi.sm@gmail.com>
2023-02-07 21:58:50 +0330

    Adding add user option to admin page

---
+++ src/components/Admin.js
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import Users from "./Users";
+
+const Admin = () => {
+    return (
+        <section>
+            <h1>Admins Page</h1>
+            <br />
+            <Users />
+            <br />
+            <Link to="/register">Add User</Link>
+            <br />
+            <div className="flexGrow">
+                <Link to="/">Home</Link>
+            </div>
+        </section>
+    )
+}
+
+export default Admin
